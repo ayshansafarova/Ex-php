9669e9d9621fd6ce6da1d5e16343e7232d936d68
Ayshan Safarova <aysensafarova@gmail.com>
2018-11-15 10:16:23 +0400

    add session

--- validation-cookies/index.php
+++ validation-cookies/index.php
@@ -7,7 +7,7 @@ session_start();
 $_SESSION['name'] = $name;
 $_SESSION['email'] = $email;
 $_SESSION['phone'] = $phone;
-session_destroy();
+// session_destroy();
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
     if (empty($_POST["name"])) {
         $nameEr = "Ad daxil edin!";
@@ -20,6 +20,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
             $count++;
         }
     }
+
+
     if (empty($_POST["email"])) {
         $emailEr = "Email daxil edin!";
     }else {
@@ -78,13 +80,13 @@ function test_input($data) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="vendors/linericon/style.css">
     <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
     <link rel="stylesheet" href="vendors/lightbox/simpleLightbox.css">
     <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
     <link rel="stylesheet" href="vendors/animate-css/animate.css">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.13/jquery.mousewheel.min.js"></script>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <section class="book_table_area section_gap">
     <div class="container">
@@ -100,15 +102,15 @@ function test_input($data) {
                     <p>Who are in extremely love with eco friendly system lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                     <form class="book_table_form row" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="form-group col-md-12">
-                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name: ex, Ayshan">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name: ex, Ayshan" value="<?php if(isset($_SESSION['name'])) echo $name ;?>">
                             <div class="error"><?php echo $nameEr ?></div>
                         </div>
                         <div class="form-group col-md-12">
-                            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email address: ex, lorem@mail.ru">
+                            <input type="email" class="form-control" id="email" name="email" placeholder="Enter email address: ex, lorem@mail.ru" value="<?php if(isset($_SESSION['email'])) echo $email ;?>">
                             <div class="error"><?php echo $emailEr ?></div>
                         </div>
                         <div class="form-group col-md-12">
-                            <input type="text" class="form-control" id="subject" name="phone" placeholder="Phone Number: ex, 0557858382">
+                            <input type="text" class="form-control" id="subject" name="phone" placeholder="Phone Number: ex, 0557858382" value="<?php if(isset($_SESSION['phone'])) echo $phone ;?>">
                             <div class="error"><?php echo $phoneEr ?></div>
                         </div>
                         <div class="form-group col-md-12">
@@ -191,9 +193,9 @@ function test_input($data) {
         box-shadow: -14px 14px 60px rgba(244, 47, 44, 0.32);
     }
 </style>
+<script src="js/bootstrap.min.js"></script>
 <script src="js/jquery-3.2.1.min.js"></script>
 <script src="js/popper.js"></script>
-<script src="js/bootstrap.min.js"></script>
 <script src="js/stellar.js"></script>
 <script src="vendors/lightbox/simpleLightbox.min.js"></script>
 <script src="vendors/nice-select/js/jquery.nice-select.min.js"></script>
